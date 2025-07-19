c5ce03b31e822e79b547b722557ca1f8635fa16a
kathleentrc <kathleen_cruz@dlsu.edu.ph>
2025-07-19 15:06:40 +0800

    add comments

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+# import pipeline
 from src.image_processing import run_yolo_detection, blur_people_and_save, print_estimated_count
 
 # input and output path
 image_path = "raw_images/cavite_extension.jpg"
 output_path = "processed_images/cavite_extension_blurred.jpg"
 
-# pipeline
+# perform each process
 img, results = run_yolo_detection(image_path)
 count = blur_people_and_save(img, results, output_path)
 print_estimated_count(count, output_path)
